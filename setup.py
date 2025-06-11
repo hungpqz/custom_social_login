b51dd3da86d8967e384af2980cda0d2ad6c0d4fc
hungpqz <quanghung.pham@gmail.com>
2025-06-11 15:02:24 +0700

    Create setup.py to upper folder

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='custom_social_login',
+    version='0.0.1',
+    description='Custom Social Login App',
+    author='HungPQ',
+    author_email='hung.pham@wellspringsaigon.edu.vn',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
